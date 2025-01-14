a67268dbfe36fb546c8e5a3873567da424535129
oscarrezab <110639308+oscarrezab@users.noreply.github.com>
2025-01-14 12:42:27 -0800

    Bare-bones UI for recommendation
    It displays the song title and similarity score of the similar songs

--- indiegems/song_recommender/urls.py
+++ indiegems/song_recommender/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recommendations/', views.get_recommendations, name='get_recommendations'),
+    path('', views.get_recommendations, name='get_recommendations'),
 ]
