1a4b591217c5062d890ae45c446387cef53fe586
oscarrezab <110639308+oscarrezab@users.noreply.github.com>
2025-01-14 12:41:30 -0800

    Simplify Recommendations View
    Move reccommendation method to SongRecommender.py;
    Call this method in views.py

--- indiegems/song_recommender/SongRecommender.py
+++ indiegems/song_recommender/SongRecommender.py
@@ -1,40 +1,52 @@
 """
-The song recommendation script. Its aim is to compute a song's similarities with the other songs in the dataset in order to recommend
+The song recommendation script. Its aim is to compute a song's similarities_map with the other songs in the dataset in order to recommend
 those with the highest scores.
 
 Created: January 2025 by Oscar Reza B.
 """
 
 import pandas as pd
-import numpy as np
-from Song import Song
 
-full_ds = pd.read_pickle('workable-dataset.pkl')
+from .Song import Song
 
-test_song = full_ds.iloc[358]
-test_object = Song(test_song['song_id'], test_song['title'], test_song['artist'], test_song['album'], test_song['album_image'], 
-                    test_song['BPM'], test_song['Camelot'], test_song['lyrics_vec'])
+class SongRecommendation():
+    def __init__(self, song_id: int, dataset_path: str = '/Users/oscarrezab/GitHub/indie-gems/indiegems/song_recommender/workable-dataset.pkl'):
+        self.dataset =  pd.read_pickle(dataset_path)
+        self.song = self.song_object_from_id(song_id)
+        self.similar_songs = self.compute_recommendations()
 
-# Initialize similarity list
-similarities = {}
-comp_3s = []
+    def get_source_song(self):
+        return self.song
+    
+    def get_similar_songs(self):
+        return self.similar_songs
+    
+    def song_object_from_id(self, song_id):
+        song_in_ds = self.dataset.iloc[song_id]  # Assumes the song id is equal to its index in the dataset
+        song_object = Song(song_in_ds['song_id'], song_in_ds['title'], song_in_ds['artist'], song_in_ds['album'], song_in_ds['album_image'], 
+                            song_in_ds['BPM'], song_in_ds['Camelot'], song_in_ds['lyrics_vec'])       
+        
+        return song_object
 
-# Get song similarities
-for song in full_ds.iloc:
-    curr_song = Song(song['song_id'], song['title'], song['artist'], song['album'], song['album_image'], 
-                        song['BPM'], song['Camelot'], song['lyrics_vec'])
-    curr_sim = test_object.similarity_to(curr_song)
-    similarities[curr_song.to_string()] = curr_sim
+    def compute_recommendations(self):
+        # Initialize similarity map and recommendations list
+        similarities_map = {}  # maps {song object : list of similarity scores}
+        recommendations = []
 
-    comp_3 = curr_sim[2]
-    comp_3s.append(comp_3)
+        # Get song similarities
+        for song in self.dataset.iloc:
+            curr_song = Song(song['song_id'], song['title'], song['artist'], song['album'], song['album_image'], 
+                                song['BPM'], song['Camelot'], song['lyrics_vec'])
+            curr_sim = self.song.similarity_to(curr_song)
+            similarities_map[curr_song] = curr_sim
 
-# Compute and print recommendation scores
-print(f"Songs similar to {test_object.to_string()}:")
-for key in similarities.keys():
-    scores = similarities.get(key)
-    rec_score = (scores[0] + scores[1] + scores[2]) / 3
-    if rec_score > 0.75:
-        print(f"{key} ({np.round(rec_score*100, 2)}%)")
-        print(f"-- similarity breakdown: (bpm: {scores[0]}  key: {scores[1]}  lyrics: {scores[2]})")
+        # Compute recommendation scores
+        for song in similarities_map.keys():
+            scores = similarities_map.get(song)
+            overall_score = (scores[0] + scores[1] + scores[2]) / 3  # computes the overall score
+            if overall_score > 0.75:
+                song.set_similarity(round(overall_score*100, 2))
+                recommendations.append(song)  # add the song object, alongside its overall score
 
+        # Return the recommendations as a response
+        return recommendations
--- indiegems/song_recommender/views.py
+++ indiegems/song_recommender/views.py
@@ -4,41 +4,11 @@ from django.http import HttpResponse
 import pandas as pd
 
 from .Song import Song
+from .SongRecommender import SongRecommendation
 
 # Create your views here.
 def get_recommendations(request):
-    full_ds = pd.read_pickle('/Users/oscarrezab/GitHub/indie-gems/indiegems/song_recommender/workable-dataset.pkl')
-
-    test_song = full_ds.iloc[358]
-    test_object = Song(test_song['song_id'], test_song['title'], test_song['artist'], test_song['album'], test_song['album_image'], 
-                        test_song['BPM'], test_song['Camelot'], test_song['lyrics_vec'])
-
-    # Initialize similarity list
-    similarities = {}
-    comp_3s = []
-
-    # Get song similarities
-    for song in full_ds.iloc:
-        curr_song = Song(song['song_id'], song['title'], song['artist'], song['album'], song['album_image'], 
-                            song['BPM'], song['Camelot'], song['lyrics_vec'])
-        curr_sim = test_object.similarity_to(curr_song)
-        similarities[curr_song.to_string()] = curr_sim
-
-        comp_3 = curr_sim[2]
-        comp_3s.append(comp_3)
-
-    # Initialize recommendations list
-    recommendations = []
-
-    # Compute and print recommendation scores
-    # print(f"Songs similar to {test_object.to_string()}:")
-    for key in similarities.keys():
-        scores = similarities.get(key)
-        rec_score = (scores[0] + scores[1] + scores[2]) / 3
-        if rec_score > 0.75:
-            # print(f"{key} ({np.round(rec_score*100, 2)}%)")
-            # print(f"-- similarity breakdown: (bpm: {scores[0]}  key: {scores[1]}  lyrics: {scores[2]})")
-            recommendations.append(key)
+    recommendations = SongRecommendation(song_id=358)
 
     # Return the recommendations as a response
-    return render(request, 'recommendations.html', {'recommendations': len(recommendations)})
+    return render(request, 'recommendations.html', {'recommendations': recommendations})
